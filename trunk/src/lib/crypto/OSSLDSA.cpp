d7381fb7a4767bf8f5b0dae9e2173f09aebba01c
Rickard Bellgrim <rickard@opendnssec.org>
2010-05-28 07:43:51 +0000

    Missed the copyright

--- trunk/src/lib/crypto/OSSLDSA.cpp
+++ trunk/src/lib/crypto/OSSLDSA.cpp
@@ -1,6 +1,9 @@
 /* $Id$ */
 
 /*
+ * Copyright (c) 2010 SURFnet bv
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
