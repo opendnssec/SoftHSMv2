b5ecffdcd345cd028547a5e55f6bd9da9a2f87b4
Jerry Lundström <jerry@opendnssec.org>
2012-10-29 10:09:12 +0000

    1.11 was dev version, take stable (1.10.3) instead.

--- testing/build-botan.sh
+++ testing/build-botan.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source `dirname "$0"`/lib.sh && init || exit 1
 
-BOTAN="Botan-1.11.0"
+BOTAN="Botan-1.10.3"
 BOTAN_URL="http://botan.randombit.net/files/$BOTAN.tgz"
 BOTAN_FILENAME="$BOTAN.tgz"
 BOTAN_HASH_TYPE="sha1"
-BOTAN_HASH="5456e77ea3510968c6f0bace2ce30b1e758e1650"
+BOTAN_HASH="0c0b2f6337d94245a32e215047311526652a114d"
 
 check_if_built botan && exit 0
 start_build botan
