ac525af3193ce048af19bf87ef03f87004cc22e6
Francis Dupont <fdupont@isc.org>
2013-09-13 15:37:41 +0200

    use main(void)

--- src/lib/crypto/test/cryptotest.cpp
+++ src/lib/crypto/test/cryptotest.cpp
@@ -33,7 +33,7 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-int main(int /*argc*/, char** /*argv*/)
+int main(void)
 {
 	CppUnit::TextUi::TestRunner runner;
 	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
