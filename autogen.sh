0b962b97d6daca9f5c78324104b5ef3ba18e38bd
halderen <berry@nlnetlabs.nl>
2019-03-05 15:31:46 +0100

    autogen failure on undefined macro AC_MSG_ERROR More extensive regeneration autoconfiguration files.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-autoreconf --install --force
+case `uname` in Darwin*) glibtoolize --copy ;;
+  *) libtoolize --copy ;; esac
+
+autoheader
+aclocal -I m4 --install
+autoconf
+
+automake --foreign --add-missing --force-missing --copy
