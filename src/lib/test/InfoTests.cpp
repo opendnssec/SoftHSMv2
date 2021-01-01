071e65c4586e282966e22e5f4bf4664e5926ccf4
Lars Silvén <lars.silven@primekey.com>
2021-01-01 01:10:22 +0100

    If the test is used for testing other PKCS#11 implementations than SofthHSM then all test that is changing SOFTHSM2_CONF will fail. So these tests will be excluded when P11M is defined. See src/lib/test/README.

--- src/lib/test/InfoTests.cpp
+++ src/lib/test/InfoTests.cpp
@@ -152,6 +152,7 @@ void InfoTests::testGetSlotInfo()
 	CRYPTOKI_F_PTR( C_Finalize(NULL_PTR) );
 }
 
+#ifndef P11M
 void InfoTests::testGetSlotInfoAlt()
 {
 	CK_RV rv;
@@ -201,6 +202,7 @@ void InfoTests::testGetSlotInfoAlt()
 	setenv("SOFTHSM2_CONF", ".\\softhsm2.conf", 1);
 #endif
 }
+#endif // P11M
 
 
 void InfoTests::testGetTokenInfo()
@@ -321,6 +323,7 @@ void InfoTests::testGetMechanismInfo()
 }
 
 
+#ifndef P11M
 void InfoTests::testGetMechanismListConfig()
 {
 	CK_RV rv;
@@ -362,6 +365,7 @@ void InfoTests::testGetMechanismListConfig()
 	setenv("SOFTHSM2_CONF", ".\\softhsm2.conf", 1);
 #endif
 }
+#endif // P11M
 
 void InfoTests::testWaitForSlotEvent()
 {
