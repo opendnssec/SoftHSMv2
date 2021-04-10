712cae0e2ea558e417e03ec61871d126ae79c1a3
Arnaud G <agrandville@gmail.com>
2021-04-11 00:00:11 +0200

    fix cppunit vcpkg autodetect

--- modules/FindCppUnit.cmake
+++ modules/FindCppUnit.cmake
@@ -9,6 +9,8 @@
 #  CPPUNIT_INCLUDE_DIR - The CppUnit include directory.
 #  CPPUNIT_LIBRARY     - The CppUnit library to link against.
 
+find_package(CppUnit CONFIG REQUIRED)
+if(NOT CppUnit_FOUND)
 FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/Test.h)
 FIND_LIBRARY(CPPUNIT_LIBRARY NAMES cppunit)
 
@@ -31,3 +33,4 @@ ELSE (CPPUNIT_FOUND)
 	ENDIF (CppUnit_FIND_REQUIRED)
 
 ENDIF (CPPUNIT_FOUND)
+ENDIF ()
